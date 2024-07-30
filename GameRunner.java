e24e8e5602bbef8b37afa0ba01094318dae31202
TylerJac <sans.gaming235@gmail.com>
2024-07-30 16:27:37 -0400

    broke

--- GameRunner.java
+++ GameRunner.java
@@ -5,9 +5,9 @@ public class GameRunner {
 
     public static void main(String[] args) {
         // play some music
-//        String filepath = "CasinoJazz.wav";
-//        PlayMusic music = new PlayMusic();
-//        music.playMusic(filepath);
+        String filepath = "CasinoJazz.wav";
+        PlayMusic music = new PlayMusic();
+        music.playMusic(filepath);
 
 
         Scanner sc = new Scanner(System.in);
